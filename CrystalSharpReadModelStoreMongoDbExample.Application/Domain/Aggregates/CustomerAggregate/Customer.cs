71530b5b009df45166c37ba940ea1a09dfadd57d
Zeeshan Mehmood <zeeshan_kyo@hotmail.com>
2024-05-13 17:02:03 +0400

    Update Customer.cs

--- CrystalSharpReadModelStoreMongoDbExample.Application/Domain/Aggregates/CustomerAggregate/Customer.cs
+++ CrystalSharpReadModelStoreMongoDbExample.Application/Domain/Aggregates/CustomerAggregate/Customer.cs
@@ -5,8 +5,8 @@ namespace CrystalSharpReadModelStoreMongoDbExample.Application.Domain.Aggregates
 {
     public class Customer : AggregateRoot<int>
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
 
         private static void ValidateCustomer(Customer customer)
         {
